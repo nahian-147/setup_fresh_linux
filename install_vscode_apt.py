f2f3773eec719240a3a77132ba24005380a1143d
kaziNAHIAN <provat44.knhp2gmail.com>
2023-04-07 01:56:32 +0600

    vscode apt installer added

---
+++ install_vscode_apt.py
@@ -0,0 +1,13 @@
+import os
+import json
+
+to_be_installed = None
+
+os.system('sudo apt update')
+os.system('sudo apt upgrade -y')
+
+with open('softwares.json') as file:
+    to_be_installed = json.load(file)
+    
+for command in to_be_installed["vscode_apt"]:
+    os.system(command)
